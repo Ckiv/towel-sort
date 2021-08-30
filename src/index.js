8254e69e1b06346f2770402cb5a01dbac5d1e25d
Ckiv <baltazar1995>
2021-08-30 18:34:12 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let sort = [];
+    if(typeof matrix != "undefined" && matrix != null && matrix.length > 0) {
+        matrix.map((data, i) => {
+            if ((i + 1) % 2 !== 0) {
+                sort = sort.concat(data)
+            } else {
+                let rev = data.reverse()
+                sort = sort.concat(rev)
+            }
+        })
+        return sort;
+    }
+    else {return sort}
 }
